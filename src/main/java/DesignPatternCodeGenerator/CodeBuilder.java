6c1b6085a952449bb8e1942fef44484ec6c1fa64
Harish238 <harishvenkat238@gmail.com>
2020-02-24 21:19:29 -0600

    added exceptions to method signature

--- src/main/java/DesignPatternCodeGenerator/CodeBuilder.java
+++ src/main/java/DesignPatternCodeGenerator/CodeBuilder.java
@@ -1,5 +1,9 @@
 package DesignPatternCodeGenerator;
 
+import org.eclipse.jface.text.BadLocationException;
+
+import java.io.IOException;
+
 public interface CodeBuilder {
-    public void writeFile();
+    public void writeFile() throws BadLocationException, IOException;
 }
