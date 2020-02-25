97f5c359f36c3eab5bb81075662d177dadf71cfc
Harish238 <harishvenkat238@gmail.com>
2020-02-24 20:27:52 -0600

    added ProductB class to AbstractFactory design pattern

---
+++ src/main/java/DesignPatterns/AbstractFactory/ProductB.java
@@ -0,0 +1,36 @@
+package DesignPatterns.AbstractFactory;
+
+import DesignPatternCodeGenerator.CodeGenerator;
+import org.eclipse.jdt.core.dom.*;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.Document;
+
+public class ProductB extends CodeGenerator {
+
+    public ProductB(String filename) {
+        super(filename);
+    }
+
+    public Document buildCode() throws BadLocationException {
+        this.createTypeDeclaration(this.fileName, false, CodeGenerator.publicKeyword,
+                false, false, null, null);
+
+        // constructor
+        MethodDeclaration constructor = this.createConstructor(this.fileName, CodeGenerator.publicKeyword);
+        Block block = this.createBlock();
+        StringLiteral argument = this.abstractSyntaxTree.newStringLiteral();
+        argument.setLiteralValue(this.fileName + " object has been instantiated.");
+        Statement printStatement = this.createPrintStatement(argument);
+        block.statements().add(printStatement);
+        constructor.setBody(block);
+
+        // add constructor to class
+        this.classDeclaration.bodyDeclarations().add(constructor);
+        // add class to CU
+        this.compilationUnit.types().add(this.classDeclaration);
+        // apply edits to the document
+        this.applyEdits();;
+        // return document
+        return this.document;
+    }
+}
