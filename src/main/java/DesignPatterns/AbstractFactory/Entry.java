bf118b4a1b1eedd014ae7aa956d01a93dfe4aba6
Harish238 <harishvenkat238@gmail.com>
2020-02-24 18:20:31 -0600

    added entry file for abstract factory design pattern

---
+++ src/main/java/DesignPatterns/AbstractFactory/Entry.java
@@ -0,0 +1,57 @@
+package DesignPatterns.AbstractFactory;
+
+import DesignPatternCodeGenerator.CodeGenerator;
+import org.eclipse.jdt.core.dom.*;
+import org.eclipse.jface.text.BadLocationException;
+import org.eclipse.jface.text.Document;
+import org.eclipse.text.edits.TextEdit;
+
+
+public class Entry extends CodeGenerator {
+
+    private String clientName, factoryName;
+
+    public Entry(String fileName, String clientName, String factoryName) {
+        super(fileName);
+        this.clientName = clientName;
+        this.factoryName = factoryName;
+    }
+
+    public Document buildCode() throws BadLocationException {
+
+        this.createTypeDeclaration(this.fileName, false, CodeGenerator.publicKeyword,
+                false, false, null, null);
+
+        // main method
+        MethodDeclaration mainMethod = this.createMainMethodDeclaration();
+        Block mainBlock = this.createBlock();
+        // client object instantiation
+        VariableDeclarationExpression clientObject = this.createVariableDeclarationExpression("client",
+                this.createSimpleType(this.clientName));
+        ClassInstanceCreation clientInstantiation = this.
+                createInstanceCreationExpression(this.createSimpleType(this.clientName));
+        ClassInstanceCreation factoryInstantiation = this.
+                createInstanceCreationExpression(this.createSimpleType(this.factoryName));
+        clientInstantiation.arguments().add(factoryInstantiation);
+        Assignment assignment = this.createAssignmentExpression(clientObject, clientInstantiation);
+        mainBlock.statements().add(this.abstractSyntaxTree.newExpressionStatement(assignment));
+        // print statement
+        MethodInvocation clientCall = this.createMethodInvocation("operation",
+                this.abstractSyntaxTree.newSimpleName("client"));
+        Statement printStatement = this.createPrintStatement(clientCall);
+        mainBlock.statements().add(printStatement);
+        // add mainBlock to main method
+        mainMethod.setBody(mainBlock);
+
+        // add mainMethod to class
+        this.classDeclaration.bodyDeclarations().add(mainMethod);
+        // add class to CU
+        this.compilationUnit.types().add(this.classDeclaration);
+        // apply edits to the document
+        this.applyEdits();
+        // return document
+        return this.document;
+
+    }
+
+}
