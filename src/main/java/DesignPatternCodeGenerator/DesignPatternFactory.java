f8726c297f38996efe3b05ebbedb6d3f35688112
Harish238 <harishvenkat238@gmail.com>
2020-02-25 18:55:41 -0600

    added instantiation of Builder design pattern in factory

--- src/main/java/DesignPatternCodeGenerator/DesignPatternFactory.java
+++ src/main/java/DesignPatternCodeGenerator/DesignPatternFactory.java
@@ -18,6 +18,7 @@ public class DesignPatternFactory {
     }
 
     private void callBuilder() throws IOException, BadLocationException {
+
         if (patternNumber == 1) {
             AbstractFactoryBuilder abstractFactoryBuilder = new AbstractFactoryBuilder(
                     this.configObject.getString("AbstractFactory.entryClass"),
@@ -28,6 +29,18 @@ public class DesignPatternFactory {
                     this.configObject.getString("AbstractFactory.productBClass")
             );
             abstractFactoryBuilder.writeFile();
+        }
+
+        else if (patternNumber == 2) {
+            BuilderBuilder builderBuilder = new BuilderBuilder(
+                    this.configObject.getString("Builder.directorClass"),
+                    this.configObject.getString("Builder.builderClass"),
+                    this.configObject.getString("Builder.complexObjectClass"),
+                    this.configObject.getString("Builder.productInterface"),
+                    this.configObject.getString("Builder.productAClass"),
+                    this.configObject.getString("Builder.productBClass")
+            );
+            builderBuilder.writeFile();
         }
     }
 
