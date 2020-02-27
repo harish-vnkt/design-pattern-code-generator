99dae8e985b85fb6a9f8f35b6202b38a35280fcb
Harish238 <harishvenkat238@gmail.com>
2020-02-27 16:23:11 -0600

    added logging and docstrings to DesignPaternFactory

--- src/main/java/DesignPatternCodeGenerator/DesignPatternFactory.java
+++ src/main/java/DesignPatternCodeGenerator/DesignPatternFactory.java
@@ -1,26 +1,57 @@
 package DesignPatternCodeGenerator;
 
-import DesignPatterns.Facade.Facade;
 import com.typesafe.config.*;
 import DesignPatternBuilders.*;
 import org.eclipse.jface.text.BadLocationException;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import java.io.IOException;
 
+/**
+ * Abstract factory for generating design patterns. This class
+ * is instantiated in the user-facing class and used to send
+ * the user input to the factory. The factory calls the appropriate
+ * builder class to build the generated code of a design pattern.
+ *
+ * @author Harish Venkataraman
+ */
 public class DesignPatternFactory {
 
+    // indicates the design pattern chosen
     private int patternNumber;
+    // object that takes user input from config file
     private Config configObject;
+    // logger object
+    private static Logger logger;
 
+    /**
+     * Constructor used to set some of the fields
+     *
+     * @param patternNumber indicates the chosen design pattern
+     * @throws IOException
+     * @throws BadLocationException
+     */
     public DesignPatternFactory(int patternNumber) throws IOException, BadLocationException {
         this.patternNumber = patternNumber;
+        // reads user input from config file
         this.configObject = ConfigFactory.load();
-        callBuilder();;
+        // instantiate logger
+        logger = LoggerFactory.getLogger("DesignPatternCodeGenerator.DesignPatternFactory");
+        // function called at instantiation
+        callBuilder();
     }
 
+    /**
+     * This function calls the appropriate builder for the chosen
+     * design pattern
+     *
+     * @throws IOException
+     * @throws BadLocationException
+     */
     private void callBuilder() throws IOException, BadLocationException {
 
         if (patternNumber == 1) {
+            logger.debug("Building Abstract Factory design pattern");
             AbstractFactoryBuilder abstractFactoryBuilder = new AbstractFactoryBuilder(
                     this.configObject.getString("AbstractFactory.entryClass"),
                     this.configObject.getString("AbstractFactory.clientClass"),
@@ -33,6 +64,7 @@ public class DesignPatternFactory {
         }
 
         else if (patternNumber == 2) {
+            logger.debug("Building Builder design pattern");
             BuilderBuilder builderBuilder = new BuilderBuilder(
                     this.configObject.getString("Builder.directorClass"),
                     this.configObject.getString("Builder.builderClass"),
@@ -45,6 +77,7 @@ public class DesignPatternFactory {
         }
 
         else if (patternNumber == 3) {
+            logger.debug("Building Factory design pattern");
             FactoryBuilder factoryBuilder = new FactoryBuilder(
                     this.configObject.getString("Factory.creatorClass"),
                     this.configObject.getString("Factory.subCreatorClass"),
@@ -55,6 +88,7 @@ public class DesignPatternFactory {
         }
 
         else if (patternNumber == 4) {
+            logger.debug("Building Facade design pattern");
             FacadeBuilder facadeBuilder = new FacadeBuilder(
                     this.configObject.getString("Facade.facadeClass"),
                     this.configObject.getString("Facade.facadeSubClass"),
@@ -69,6 +103,7 @@ public class DesignPatternFactory {
         }
 
         else if (patternNumber == 5) {
+            logger.debug("Building Chain of Responsibility design pattern");
             ChainOfResponsibilityBuilder corBuilder = new ChainOfResponsibilityBuilder(
                     this.configObject.getString("ChainOfResponsibility.senderClass"),
                     this.configObject.getString("ChainOfResponsibility.handlerClass"),
@@ -79,6 +114,7 @@ public class DesignPatternFactory {
         }
 
         else if (patternNumber == 6) {
+            logger.debug("Building Mediator design pattern");
             MediatorBuilder mediatorBuilder = new MediatorBuilder(
                     this.configObject.getString("Mediator.mediatorInterface"),
                     this.configObject.getString("Mediator.mediatorClass"),
@@ -90,6 +126,7 @@ public class DesignPatternFactory {
         }
 
         else if (patternNumber == 7) {
+            logger.debug("Building Visitor design pattern");
             VisitorBuilder visitorBuilder = new VisitorBuilder(
                     this.configObject.getString("Visitor.clientClass"),
                     this.configObject.getString("Visitor.visitorAbstractClass"),
