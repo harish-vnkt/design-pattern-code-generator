5575fe1da8e9b5da259dabc277e31f865248e3c5
Harish238 <harishvenkat238@gmail.com>
2020-02-27 16:07:54 -0600

    added logging docstrings to CodeBuilder

--- src/main/java/DesignPatternCodeGenerator/CodeBuilder.java
+++ src/main/java/DesignPatternCodeGenerator/CodeBuilder.java
@@ -4,6 +4,14 @@ import org.eclipse.jface.text.BadLocationException;
 
 import java.io.IOException;
 
+/**
+ * Interface implemented by builder classes of each design pattern.
+ * The builder classes instantiate the individual CodeGenerators in each
+ * design pattern and build the code, finally writing them to file.
+ *
+ * @author Harish Venkataraman
+ */
 public interface CodeBuilder {
+    // function to be implemented
     public void writeFile() throws BadLocationException, IOException;
 }
