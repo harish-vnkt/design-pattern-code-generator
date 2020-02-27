05888f07a4ffc7c591ea2836d2d1ff38aa03f4a7
Harish238 <harishvenkat238@gmail.com>
2020-02-27 01:11:14 -0600

    added builder class for Visitor design pattern

---
+++ src/main/java/DesignPatternBuilders/VisitorBuilder.java
@@ -0,0 +1,64 @@
+package DesignPatternBuilders;
+
+import DesignPatternCodeGenerator.CodeBuilder;
+import DesignPatterns.Visitor.*;
+import org.apache.commons.io.FileUtils;
+import org.eclipse.jface.text.BadLocationException;
+
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+import java.io.File;
+
+public class VisitorBuilder implements CodeBuilder {
+
+    String clientClass, visitorClass, visitorConcreteClass, elementClass, element1Class, element2Class;
+    String directoryPath = "generated_patterns/Visitor/";
+
+    public VisitorBuilder(String clientClass, String visitorClass, String visitorConcreteClass,
+                          String elementClass, String element1Class, String element2Class) {
+        this.clientClass = clientClass;
+        this.visitorClass = visitorClass;
+        this.visitorConcreteClass = visitorConcreteClass;
+        this.elementClass = elementClass;
+        this.element1Class = element1Class;
+        this.element2Class = element2Class;
+    }
+
+    public void writeFile() throws BadLocationException, IOException {
+
+        // create file objects
+        Client clientObject = new Client(this.clientClass, this.visitorClass, this.visitorConcreteClass,
+                this.element1Class, this.element2Class);
+        Visitor visitorObject = new Visitor(this.visitorClass, this.element1Class, this.element2Class);
+        VisitorConcrete visitorConcreteObject = new VisitorConcrete(this.visitorConcreteClass,
+                this.visitorClass, this.element1Class, this.element2Class);
+        Element elementObject = new Element(this.elementClass, this.visitorClass);
+        ElementConcrete elementConcreteObject1 = new ElementConcrete(this.element1Class,
+                this.visitorClass, this.elementClass);
+        ElementConcrete elementConcreteObject2 = new ElementConcrete(this.element2Class,
+                this.visitorClass, this.elementClass);
+
+        // write the generated code
+        String clientFilename = this.directoryPath + clientObject.fileName + ".java";
+        FileUtils.writeStringToFile(new File(clientFilename),
+                clientObject.buildCode().get(), StandardCharsets.UTF_8);
+        String visitorFilename = this.directoryPath + visitorObject.fileName + ".java";
+        FileUtils.writeStringToFile(new File(visitorFilename),
+                visitorObject.buildCode().get(), StandardCharsets.UTF_8);
+        String visitorConcreteFilename = this.directoryPath + visitorConcreteObject.fileName + ".java";
+        FileUtils.writeStringToFile(new File(visitorConcreteFilename),
+                visitorConcreteObject.buildCode().get(), StandardCharsets.UTF_8);
+        String elementFilename = this.directoryPath + elementObject.fileName + ".java";
+        FileUtils.writeStringToFile(new File(elementFilename),
+                elementObject.buildCode().get(), StandardCharsets.UTF_8);
+        String elementConcreteFilename1 = this.directoryPath + elementConcreteObject1.fileName + ".java";
+        FileUtils.writeStringToFile(new File(elementConcreteFilename1),
+                elementConcreteObject1.buildCode().get(), StandardCharsets.UTF_8);
+        String elementConcreteFilename2 = this.directoryPath + elementConcreteObject2.fileName + ".java";
+        FileUtils.writeStringToFile(new File(elementConcreteFilename2),
+                elementConcreteObject2.buildCode().get(), StandardCharsets.UTF_8);
+
+    }
+
+}
