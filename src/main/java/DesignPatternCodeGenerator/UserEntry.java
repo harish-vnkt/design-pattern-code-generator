7273d0d603bf7f606b3fba1896e8fd511193cc0f
Harish238 <harishvenkat238@gmail.com>
2020-02-27 17:19:26 -0600

    final UserEntry class

--- src/main/java/DesignPatternCodeGenerator/UserEntry.java
+++ src/main/java/DesignPatternCodeGenerator/UserEntry.java
@@ -1,7 +1,30 @@
 package DesignPatternCodeGenerator;
 
+import org.eclipse.jface.text.BadLocationException;
+
+import java.io.IOException;
+import java.util.Scanner;
+
 public class UserEntry {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException, BadLocationException {
+        while(true) {
+            try {
+                System.out.println("Select design pattern to generate\n" +
+                        "1 - Abstract Factory\n" +
+                        "2 - Builder\n" +
+                        "3 - Chain of Responsibility\n" +
+                        "4 - Facade\n" +
+                        "5 - Factory\n" +
+                        "6 - Mediator\n" +
+                        "7 - Visitor\n");
+                Scanner scanner = new Scanner(System.in);
+                int i = scanner.nextInt();
+                new DesignPatternFactory(i);
+                break;
+            }
+            catch (Exception e) {
+                System.out.println("Enter again\n");
+            }
+        }
     }
 }
