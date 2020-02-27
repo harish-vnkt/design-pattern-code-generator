5d584722567702ac09fb0662140f45a4b374d5be
Harish238 <harishvenkat238@gmail.com>
2020-02-27 16:44:10 -0600

    added logging and docstrings to AbstractFactoryBuilder

--- src/main/java/DesignPatternBuilders/AbstractFactoryBuilder.java
+++ src/main/java/DesignPatternBuilders/AbstractFactoryBuilder.java
@@ -9,14 +9,24 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 
 import java.io.File;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+/**
+ * Builder for Abstract Factory design pattern
+ *
+ */
 public class AbstractFactoryBuilder implements CodeBuilder {
 
+    // user input
     String entryClass, clientClass, factoryClass, factoryInterface, productAClass, productBClass;
+    private Logger logger;
+    // output directory
     String directoryPath = "generated_patterns/AbstractFactory/";
 
     public AbstractFactoryBuilder(String entryClass, String clientClass, String factoryClass,
                                   String factoryInterface, String productAClass, String productBClass) {
+        this.logger = LoggerFactory.getLogger("DesignPatternBuilders.AbstractFactory");
         this.entryClass = entryClass;
         this.clientClass = clientClass;
         this.factoryClass = factoryClass;
@@ -38,19 +48,25 @@ public class AbstractFactoryBuilder implements CodeBuilder {
         ProductB productBObject = new ProductB(this.productBClass);
 
         // writing the generated code
+        logger.debug("Creating Entry file");
         String entryFilename = this.directoryPath + entryObject.fileName + ".java";
         FileUtils.writeStringToFile(new File(entryFilename), entryObject.buildCode().get(), StandardCharsets.UTF_8);
+        logger.debug("Creating Client file");
         String clientFilename = this.directoryPath + clientObject.fileName + ".java";
         FileUtils.writeStringToFile(new File(clientFilename), clientObject.buildCode().get(), StandardCharsets.UTF_8);
+        logger.debug("Creating AbstractFactory file");
         String abstractFactoryFilename = this.directoryPath + abstractFactoryObject.fileName + ".java";
         FileUtils.writeStringToFile(new File(abstractFactoryFilename), abstractFactoryObject.buildCode().get(),
                 StandardCharsets.UTF_8);
+        logger.debug("Creating ConcreteFactory file");
         String factoryFilename = this.directoryPath + factoryObject.fileName + ".java";
         FileUtils.writeStringToFile(new File(factoryFilename), factoryObject.buildCode().get(),
                 StandardCharsets.UTF_8);
+        logger.debug("Creating ProductA file");
         String productAFilename = this.directoryPath + productAObject.fileName + ".java";
         FileUtils.writeStringToFile(new File(productAFilename), productAObject.buildCode().get(),
                 StandardCharsets.UTF_8);
+        logger.debug("Creating ProductB file");
         String productBFilename = this.directoryPath + productBObject.fileName + ".java";
         FileUtils.writeStringToFile(new File(productBFilename), productBObject.buildCode().get(),
                 StandardCharsets.UTF_8);
